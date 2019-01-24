7b1e3a114779040bcabf4c2305faeb848500b063
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-24 08:33:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,9 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end
 
-  array.each {|name| badge_maker(name)}
+def batch_badge_creator
+  #array.each {|name| badge_maker(name)}
+  array.each do |name|
+    badge_maker(name)
+  end
+end
