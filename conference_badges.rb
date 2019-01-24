fcc2e05ea99c2cca1b6e4a76c1db1705598d1863
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-24 08:57:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
+  new_array = []
     array.each_with_index do |name, index|
     return "Hello #{name}. You'll be assigned to room #{index + 1}."
   end
